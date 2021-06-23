38a4b149e948ad00da1231cafb1b7f69576a8fe9
phobo99 <phobotkcb@gmail.com>
2021-06-23 20:38:02 +0700

    [Buổi 1] Javascript căn bản

---
+++ Lesson1/main.js
@@ -0,0 +1,24 @@
+let user = {
+    name: "Pho",
+    birthday: "01-02-2021",
+    email: "pho@acbas.com",
+    phone: "1782534781"
+}
+console.log(user)
+
+
+function multiple(a, b) {
+    return a * b;
+}
+
+console.log(multiple(5, 5))
+
+
+let arr = [1, 2, 3, 4, 5, 65, 6, 123];
+
+function sum(arr, i, j) {
+    return arr[i] + arr[j];
+
+}
+
+console.log(sum(arr, 6, 7))
